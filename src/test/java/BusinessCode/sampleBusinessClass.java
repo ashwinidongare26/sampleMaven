5bd559ee3a3ce9ef1083b61334846d64910613d5
ashwinidongare26 <dongare,ashwini@gmail.com>
2022-09-30 15:42:05 +0530

    Bussines class commit

---
+++ src/test/java/BusinessCode/sampleBusinessClass.java
@@ -0,0 +1,11 @@
+package BusinessCode;
+
+public class sampleBusinessClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+	}
+
+}
